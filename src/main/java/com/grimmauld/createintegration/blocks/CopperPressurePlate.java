faaf2dea4a40efa437ec2031ed4cc0e8afe4c2c9
LordGrimmauld <soeren@benjos.de>
2020-05-21 00:46:27 +0200

    added copper pressure plate oxidizing, cleaned up model files for pressure plates

--- src/main/java/com/grimmauld/createintegration/blocks/CopperPressurePlate.java
+++ src/main/java/com/grimmauld/createintegration/blocks/CopperPressurePlate.java
@@ -1,23 +1,75 @@
 package com.grimmauld.createintegration.blocks;
 
+import java.util.LinkedList;
+import java.util.OptionalDouble;
+import java.util.Random;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.WeightedPressurePlateBlock;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 public class CopperPressurePlate extends WeightedPressurePlateBlock{
+	public static final IntegerProperty OXIDIZATION = IntegerProperty.create("oxidization", 0, 7);
+	
 	public CopperPressurePlate(){
 		super(15, Block.Properties.from(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE));
 		setRegistryName("copper_pressure_plate");
+		setDefaultState(getDefaultState().with(OXIDIZATION, 0));
 	}
 
-	
+	@Override
+	protected void fillStateContainer(Builder<Block, BlockState> builder) {
+		super.fillStateContainer(builder.add(OXIDIZATION));
+	}
+
+	@Override
+	public boolean ticksRandomly(BlockState state) {
+		return super.ticksRandomly(state) || state.get(OXIDIZATION) < 7;
+	}
+
+	@Override
+	public void randomTick(BlockState state, World worldIn, BlockPos pos, Random random) {
+		if (worldIn.getRandom().nextFloat() <= 1 / 32f) {
+			int currentState = state.get(OXIDIZATION);
+			boolean canIncrease = false;
+			LinkedList<Integer> neighbors = new LinkedList<>();
+			for (Direction facing : Direction.values()) {
+				BlockPos neighbourPos = pos.offset(facing);
+				if (!worldIn.isBlockPresent(neighbourPos))
+					continue;
+				BlockState neighborState = worldIn.getBlockState(neighbourPos);
+				if (neighborState.has(OXIDIZATION) && neighborState.get(OXIDIZATION) != 0) {
+					neighbors.add(neighborState.get(OXIDIZATION));
+				}
+				if (Block.hasSolidSide(neighborState, worldIn, neighbourPos, facing.getOpposite())) {
+					continue;
+				}
+				canIncrease = true;
+			}
+			if (canIncrease) {
+				OptionalDouble average = neighbors.stream().mapToInt(v -> v).average();
+				if (average.orElse(7d) >= currentState)
+					worldIn.setBlockState(pos, state.with(OXIDIZATION, Math.min(currentState + 1, 7)));
+			}
+		}
+	}
+
+	@Override
+	public float getBlockHardness(BlockState blockState, IBlockReader worldIn, BlockPos pos) {
+		return this.blockHardness - 0.2f * blockState.get(OXIDIZATION);
+	}
 	
 	@Override
 	protected int computeRedstoneStrength(World worldIn, BlockPos pos) {
